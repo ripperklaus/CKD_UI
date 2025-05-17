b83cca61f2dbc07e48cbe441035db09f269155e1
Udityanshu pandey <104460339+ripperklaus@users.noreply.github.com>
2025-05-17 19:57:06 +0530

    added into int

--- src/api/predictCkd.js
+++ src/api/predictCkd.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export async function predictCKD(form) {
   const formatted = {};
   for (const key in form) {
-    if (["rbc", "pc", "pcc", "ba", "htn", "dm", "cad", "appet", "pe", "ane"].includes(key)) {
+    if (["rbc", "pc", "pcc", "ba", "htn", "dm", "cad", "appet", "pe", "ane","pvc","wc"].includes(key)) {
       formatted[key] = parseInt(form[key]);
     } else {
       formatted[key] = parseFloat(form[key]);
